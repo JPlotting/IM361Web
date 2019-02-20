76b39e347141d4efa48bfdad2eacae959aa40f2b
Jakob Plotts <jakobplotts@yahoo.com>
2019-02-19 22:16:17 -0600

    Completed Assignment for Wednesday

--- src/index.js
+++ src/index.js
@@ -5,11 +5,39 @@ var app = new Vue({
   el: '#app',
 
   data: {
+    pageName: 'article',
+    article: {
+      title: '3 Graphic Design Tips for Non-Designers',
+      author: 'Midori Nediger',
+      date: 'October 12, 2018',
+      likes: 0,
+      isLiked: false
+    },
 
+    commentText: '',
+    comments: []
   },
 
   methods: {
+    likify: function( comment,event ) {
+      comment.isLiked = true;
+      comment.likes++;
+    },
 
+    unlikify: function( comment,event ) {
+      comment.isLiked = false;
+      comment.likes--;
+    },
+
+    processComment: function( event ) {
+      this.comments.push( {
+        userName: "Jakob the Snakob",
+        text: this.commentText,
+        likes: 0,
+        isLiked: false
+      } );
+      this.commentText = '';
+    }
   }
 });
 
