cb3d26544115a089ba9dd8ee2ee11ecde5ee2130
Jakob Plotts <jakobplotts@yahoo.com>
2019-02-24 23:50:13 -0600

    Vue.js updates for Feb 25th class

--- src/index.js
+++ src/index.js
@@ -5,14 +5,33 @@ var app = new Vue({
   el: '#app',
 
   data: {
-    pageName: 'article',
+    pageName: 'browse',
+    lastPageNames: [],
+
     article: {
-      title: '3 Graphic Design Tips for Non-Designers',
-      author: 'Midori Nediger',
-      date: 'October 12, 2018',
+      image: '',
+      title: '',
+      author: '',
+      date: '',
       likes: 0,
       isLiked: false
     },
+
+    articles: [ {
+        image: 'https://images.pexels.com/photos/1455985/pexels-photo-1455985.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        title: '3 Graphic Design Tips for Non-Designers',
+        author: 'Midori Nediger',
+        date: 'October 12, 2018',
+        likes: 0,
+        isLiked: false
+      }, {
+        image: 'https://images.pexels.com/photos/1167021/pexels-photo-1167021.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        title: 'some other title',
+        author: 'some other author',
+        date: 'some other date',
+        likes: 0,
+        isLiked: false
+    } ],
 
     commentText: '',
     comments: []
@@ -36,8 +55,44 @@ var app = new Vue({
         likes: 0,
         isLiked: false
       } );
+
       this.commentText = '';
+    },
+
+    goToAPage: function( page,event ) {
+      if (page != this.pageName) {
+        this.lastPageNames.push( this.pageName );
+        this.pageName = page;
+      }
+    },
+
+    goBackAPage: function( event ) {
+      if (this.lastPageNames.length > 0) {
+        this.pageName = this.lastPageNames.pop();
+      } else {
+        // something to redirect to last website before entering this one
+      }
+    },
+
+    setArticle: function( result,event ) {
+      this.article.image = result.image;
+      this.article.title = result.title;
+      this.article.author = result.author;
+      this.article.date = result.date;
+      this.article.likes = result.likes;
+      this.article.isLiked = result.isLiked;
+
+      this.goToAPage( 'article',event );
     }
+  },
+
+  created: function() {
+    // WOW this really won't work? Love it!
+    document.addEventListener( "backbutton",this.goBackAPage,false );
+  },
+
+  beforeDestroy: function() {
+    document.removeEventListener( "backbutton",this.goBackAPage );
   }
 });
 
